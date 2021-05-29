db85f802b6edfa523d44fd3ab63450437c28d856
luutrang308 <luutrangk10@gmail.com>
2021-05-29 10:43:42 +0700

    quaquy

--- 2_webhanoia/assets/js/custrom-tr.js
+++ 2_webhanoia/assets/js/custrom-tr.js
@@ -19,8 +19,8 @@ $(document).ready(function(){
   $('.slider_banner').slick({
     dots: true,
     infinite: true,
-    speed: 300,
-    autoplaySpeed: 1200,
+    speed: 3000,
+    autoplaySpeed: 3000,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
@@ -52,6 +52,46 @@ $(document).ready(function(){
   var crl3 = circlr('circlr3', {
     scroll : true,
   });*/
+
+  $('.slider_for_pro').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    centerMode: true,
+    asNavFor: '.slider_nav_pro',
+    
+  });
+  $('.slider_nav_pro').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider_for_pro',
+    dots: false,
+    arrows: true,
+    centerMode: false,
+    focusOnSelect: true,
+    /*prevArrow: "<button class='prev slick-prev'><img class='left-arrow ' src='./assets/img/left.png' alt=''></button>",
+    nextArrow: "<button class='next slick-next'><img class='right-arrow ' src='./assets/img/right1.png' alt=''></button>",
+    */
+  });
+
+  $('.element_relate_pro').slick({
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+  });
+
+
+
+
+
+
 
 });
 
